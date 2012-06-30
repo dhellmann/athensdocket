57435deabf98aa4a43b661e38007a4344db3a2e5
Doug Hellmann <doug.hellmann@gmail.com>
2012-06-30 12:35:33 -0400

    track the active navbar page and highlight it correctly
    Change-Id: I28d4d515a9057a2ccb7d33e24a65f80e8beadcce

--- docket/server.py
+++ docket/server.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template
+import functools
+
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
 
+def set_navbar_active(f):
+    "Set the navbar active value to the name of the wrapped function."
+    @functools.wraps(f)
+    def set_active(*args, **kwds):
+        g.navbar_active = f.__name__
+        return f(*args, **kwds)
+    return set_active
+
+
 @app.route('/')
+@set_navbar_active
 def index():
     return render_template('index.html')
 
 
 @app.route('/about')
+@set_navbar_active
 def about():
     return render_template('about.html')
 
 
 @app.route('/search')
+@set_navbar_active
 def search():
     return render_template('search.html')
 
