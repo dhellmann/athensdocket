651fcd55cbfb62b461a5380205ff4aedcbb85016
Doug Hellmann <doug.hellmann@gmail.com>
2012-06-30 11:55:46 -0400

    add bootstrap files and stflesh out the home page template
    Change-Id: I5967b62d7c64b6e547ad440cd276c5c25a2a3ccc

--- docket/server.py
+++ docket/server.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
